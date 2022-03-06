4a14caf8e2dd7b37f3f1032e48e54ba0cf8c42b1
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-03-06 14:35:03 -0600

    psuedocoded javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+var countdown = document.getElementById("timer")
+var questionsArr = document.getElementsByClassName("questionsArr")
+var button = document.getElementsByClassName("start-btn")
+//questions: "what is my name?", 
+//choices: ["Lisa", "Tommy", "Monica"],
+//answer: "Monica"
+
+// click handler 
+//countdowntimer
+// choices display choices
+// questions 
+//question array variable make sure syntax is right 
+// make sure questions and answers display after start button
+// make sure the for loop works 
